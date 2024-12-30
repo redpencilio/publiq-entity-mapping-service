26dc95dd1358b3f32c8b6f7ad74ee0f9e8508280
Michaël Dierick <michael.dierick@redpencil.io>
2024-12-30 11:18:28 +0100

    improve logging and method doc

--- web.py
+++ web.py
@@ -7,8 +7,13 @@ from helpers import logger
 from load_addresses import load_addresses
 from mapping import write_mapping, check_mapping_existence
 
-@app.route("/hello")
-def hello():
+@app.route("/full-address-mapping")
+def map_all_addresses():
+    """
+    Load all addresses,
+    Calculate mappings between them,
+    Write mappings that aren't in the DB yet.
+    """
     addresses = load_addresses()
 
     addresses_by_full = {a.full_address: a for a in addresses}
@@ -21,13 +26,13 @@ def hello():
                 class_match = addresses_by_full[potential_match[0]]
                 score = address.score(class_match)
                 if score > 50:
-                    print(f"-------\nMatch for \n{address.full_address}")
-                    print(f"{str(potential_match[0])} full score {address.score(class_match)}")
+                    logger.debug(f"-------\nMatch for \n{address.full_address}")
+                    logger.debug(f"{str(potential_match[0])} full score {address.score(class_match)}")
                     existing_mapping = check_mapping_existence(address.uri, class_match.uri)
                     if existing_mapping:
                         logger.info(f"Match already has been recorded previously. Full score was {existing_mapping['similarity_score']}. Skipping")
                     else:
-                        logger.debug(f"Writing match to DB")
+                        logger.info(f"Writing match to DB")
                         write_mapping(address.uri,
                                     address.full_address,
                                     class_match.uri,
